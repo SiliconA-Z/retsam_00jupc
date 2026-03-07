3844a7f995eaf1b586f32b74b822e4d9e1a353b1
AZero13 <83477269+SiliconA-Z@users.noreply.github.com>
2026-03-07 11:28:45 -0500

    dead code comment out

--- src/wazaeffect/wsp_steff.c
+++ src/wazaeffect/wsp_steff.c
@@ -226,7 +226,7 @@ static void StatusEffect_BG_SetUp(WE_SYS_PTR wsp, STEFF_PARAM* param)
 	{
 		int pri;
 		
-		pri = BT_BGPriorityGet(wk->ts.wsp, WES_BF_BACKGROUND);
+		//pri = BT_BGPriorityGet(wk->ts.wsp, WES_BF_BACKGROUND);
 		pri = 2;
 		
 		CATS_ObjectBGPriSetCap(wk->cap_n, pri);
